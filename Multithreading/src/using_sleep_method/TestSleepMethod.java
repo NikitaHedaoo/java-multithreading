4ae6fa93f18365e85683f8928ce990b296668730
NikitaHedaoo <nh2296@gmail.com>
2024-03-05 21:49:19 +0530

    testing sleep()method with multiple threads

---
+++ Multithreading/src/using_sleep_method/TestSleepMethod.java
@@ -0,0 +1,38 @@
+package using_sleep_method;
+
+//Thread.sleep causes the current thread to suspend execution for a specified period. 
+//This is an efficient means of making processor time available to the other threads 
+//of an application or other applications that might be running on a computer system.
+
+class MyThread1 extends Thread{
+	
+	public void run() {
+		for(int i=1; i<=10 ; i++) {
+			try{
+				Thread.sleep(250);
+			}catch(InterruptedException e) {
+				System.out.println(e);
+			}
+			System.out.println(Thread.currentThread().getName());
+		}
+	}
+}
+public class TestSleepMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyThread1 mT1 = new MyThread1();
+		mT1.start();
+		MyThread1 mT2 = new MyThread1();
+		mT2.start();
+		for(int i=1; i<=10 ; i++) {
+			try{
+				Thread.sleep(250);
+			}catch(InterruptedException e) {
+				System.out.println(e);
+			}
+			System.out.println(Thread.currentThread().getName());
+		}
+	}
+
+}
